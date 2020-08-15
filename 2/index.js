936b9c67f915ad1c210979ec4bbeb2c6f18ea919
ShuoyuGong <2788696894@qq.com>
2020-08-15 21:49:10 +0800

    once

--- 2/index.js
+++ 2/index.js
@@ -1,12 +1,18 @@
-var http = require('http')
-var io = require('socket.io')
-var server = http.createServer((req, res) => {})
+// var http = require('http')
+// var io = require('socket.io')
+// var server = http.createServer((req, res) => {})
 
-// 监听
-server.listen(2183)
-io.listen(server).on('connection', (user) => {
-  user.on('div_node', (json) => {
-    user.broadcast.emit('style', json)
-    // console.log(json)
-  })
-})
+// // 监听
+// server.listen(2183)
+// io.listen(server).on('connection', (user) => {
+//   user.on('div_node', (json) => {
+//     user.broadcast.emit('style', json)
+//     // console.log(json)
+//   })
+// })
+
+var a = '2342'
+function setName() {
+  var a = '4536'
+  console.log(a)
+}
--- QQ/index.js
+++ QQ/index.js
@@ -1,40 +1,75 @@
-var http = require('http')
-var io = require('socket.io')
-var server = http.createServer((req, res) => {})
+// var http = require('http')
+// var io = require('socket.io')
+// var server = http.createServer((req, res) => {})
 
-// 监听
-server.listen(2183)
-var arrUser = []
-var arrMsg = []
-io.listen(server).on('connection', (user) => {
-  // 登陆获取用户名
-  user.on('name', (name) => {
-    arrUser.push(name)
-    user.lastNames = name
-    user.broadcast.emit('userLogin', arrUser)
-    user.emit('userLogin', arrUser)
-    user.emit('agoMsgs', arrMsg)
-  })
+// // 监听
+// server.listen(2183)
+// var arrUser = []
+// var arrMsg = []
+// io.listen(server).on('connection', (user) => {
+//   // 登陆获取用户名
+//   user.on('name', (name) => {
+//     arrUser.push(name)
+//     user.lastNames = name
+//     user.broadcast.emit('userLogin', arrUser)
+//     user.emit('userLogin', arrUser)
+//     user.emit('agoMsgs', arrMsg)
+//   })
 
-  user.on('msg', (msg) => {
-    arrMsg.push({
-      userName: user.lastNames,
-      timer: new Date().toLocaleTimeString(),
-      msg: msg,
-    })
-    user.broadcast.emit('msg', {
-      userName: user.lastNames,
-      timer: new Date().toLocaleTimeString(),
-      msg: msg,
-    })
-    user.emit('myMsg', {
-      userName: user.lastNames,
-      timer: new Date().toLocaleTimeString(),
-      msg: msg,
-    })
-  })
-  user.on('disconnect', () => {
-    arrUser.splice(arrUser.indexOf(user.lastNames), 1)
-    user.broadcast.emit('userDisconnect', user.lastNames)
-  })
+//   user.on('msg', (msg) => {
+//     arrMsg.push({
+//       userName: user.lastNames,
+//       timer: new Date().toLocaleTimeString(),
+//       msg: msg,
+//     })
+//     user.broadcast.emit('msg', {
+//       userName: user.lastNames,
+//       timer: new Date().toLocaleTimeString(),
+//       msg: msg,
+//     })
+//     user.emit('myMsg', {
+//       userName: user.lastNames,
+//       timer: new Date().toLocaleTimeString(),
+//       msg: msg,
+//     })
+//   })
+//   user.on('disconnect', () => {
+//     arrUser.splice(arrUser.indexOf(user.lastNames), 1)
+//     user.broadcast.emit('userDisconnect', user.lastNames)
+//   })
+// })
+
+// var mysql = require('mysql')
+// var connection = mysql.createConnection({
+//   host: '127.0.0.1',
+//   user: 'root',
+//   password: '1763034gong',
+//   database: 'socket',
+// })
+// connection.connect()
+// var sql = 'SELECT * FROM user WHERE id = 2'
+// //查
+// connection.query(sql, function (err, result) {
+//   if (err) {
+//     console.log('[SELECT ERROR] - ', err.message)
+//     return
+//   }
+//   console.log(result)
+// })
+
+// connection.end()
+
+//express_demo.js 文件
+var express = require('express')
+var app = express()
+
+app.get('/index', function (req, res) {
+  res.send('Hello World')
+})
+
+var server = app.listen(8081, function () {
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log('应用实例，访问地址为 http://127.0.0.5', host, port)
 })
